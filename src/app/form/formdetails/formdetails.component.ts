b1e83b15a7d5eb11fbd74752a6cca2eb18737990
Krupa Patel <53213815+krupa253@users.noreply.github.com>
2022-04-05 19:50:00 +0530

    all done

--- src/app/form/formdetails/formdetails.component.ts
+++ src/app/form/formdetails/formdetails.component.ts
@@ -1,6 +1,5 @@
-import { NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators, FormArray, AbstractControl } from '@angular/forms';
+import { FormBuilder, Validators, FormArray, AbstractControl, FormControl } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 // import { ConfirmedValidator } from '../Validator/confirmed.validator';
 
@@ -16,73 +15,112 @@ interface Type {
 })
 export class FormdetailsComponent implements OnInit {
   formdetails!: any;
-  
+
   detailForm!: any;
   mode: any;
   user: any = {};
   FORM_CONSTANT = 'form_data';
+  answer1: any;
+  answer2: any;
 
-  constructor( private formBuilder: FormBuilder, private route1: Router, private route: ActivatedRoute ) {
+  constructor(private formBuilder: FormBuilder, private route1: Router, private route: ActivatedRoute) {
+    this.formdetails = this.formBuilder.group({
+      details: this.formBuilder.array([])
+    });
     this.route.params.subscribe((res) => {
       this.mode = res['mode'];
       console.log(this.mode);
-      if(this.mode === 'edit'){
-        this.patchFormValue() 
+      if (this.mode === 'edit') {
+        this.patchFormValue()
+      }
+      else {
+        this.addDetail();
+
       }
     })
   }
 
   patchFormValue() {
     let users = JSON.parse(localStorage.getItem(this.FORM_CONSTANT)!);
+    console.log(users);
     if (users) {
-    this.formdetails.patchValue(users);
-  }
+      this.formdetails.patchValue(users);
+      this.patchform();
+    }
   }
 
   ngOnInit(): void {
-    this.formdetails = this.formBuilder.group({
-      details: this.formBuilder.array([])
-    });
-    this.addDetail();
+
   }
 
   get details() {
     return this.formdetails.controls["details"] as FormArray;
   }
 
-  addDetail() {
-    this.detailForm  = this.formBuilder.group({
-      question: ['', [Validators.required]],
-      answer_type: ['', [Validators.required]],
-      answer1: ['', [Validators.required]],
-      answer2: ['', [Validators.required]]
-    }, { 
-      // validator: ConfirmedValidator('answer_type', 'answer1', 'answer2')
-    });
-    this.details.push(this.detailForm );
+  patchform() {
+    const user = JSON.parse(localStorage.getItem(this.FORM_CONSTANT)!);
+    if (user.details) {
+      for (let index = 0; index < user.details.length; index++) {
+        const element = user.details[index];
+        console.log('element: ', element);
+        this.detailForm = this.formBuilder.group({
+          question: [element.question, [Validators.required]],
+          answer_type: [element.answer_type, [Validators.required]],
+          answer1: [element.answer1],
+          answer2: [element.answer2]
+        });
+      this.details.push(this.detailForm);
+      }
+    }
+
   }
+
+  addDetail() {
+      this.detailForm = this.formBuilder.group({
+        question: ['', [Validators.required]],
+        answer_type: ['', [Validators.required]],
+        answer1: [''],
+        answer2: ['']
+      });
+      this.details.push(this.detailForm);
+  
+
+    this.detailForm.get('answer_type').valueChanges.subscribe( (id: number) => {
+      if (id === 3) {
+        this.detailForm.get('answer1').setValidators([Validators.required]);
+        this.detailForm.get('answer2').setValidators([Validators.required]);
+      } else {
+        this.detailForm.get('answer1').setValidators(null);
+        this.detailForm.get('answer2').setValidators(null);
+      }
+      this.detailForm.get('answer1').updateValueAndValidity();
+      this.detailForm.get('answer2').updateValueAndValidity();
+    });
+
+  }
+
 
   deleteDetail(detailIndex: number) {
     this.details.removeAt(detailIndex);
   }
 
   onSubmit() {
-    // if (this.formdetails.valid) {
-
-
-        if (localStorage.getItem(this.FORM_CONSTANT)) {
-          localStorage.removeItem(this.FORM_CONSTANT);  
-        }
-        localStorage.setItem(this.FORM_CONSTANT, JSON.stringify(this.formdetails.value));
-        this.route1.navigate(['/']);
-    // }
- 
+    if (this.formdetails.valid) {
+      if (localStorage.getItem(this.FORM_CONSTANT)) {
+        localStorage.removeItem(this.FORM_CONSTANT);
+      }
+      localStorage.setItem(this.FORM_CONSTANT, JSON.stringify(this.formdetails.value));
+      this.route1.navigate(['/']);
+    }
   }
 
-  types : Type[] = [
-    { id : 1, Value : 'Short Text' },
-    { id : 2, Value : 'Long Text' },
-    { id : 3, Value : 'Radio Button' }
-  ];
+  reset() {
+    this.formdetails.reset();
+  }
 
+  types: Type[] = [
+    { id: 1, Value: 'Short Text' },
+    { id: 2, Value: 'Long Text' },
+    { id: 3, Value: 'Radio Button' }
+  ];
 }
